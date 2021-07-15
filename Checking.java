25fc47b9617b98d532e9a40c05a20a1f9be145bd
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-07-15 15:47:07 -0600

    Create Checking.java

---
+++ Checking.java
@@ -0,0 +1,19 @@
+package kanishka_roy;
+import java.io.*;
+import java.util.*;
+public class Checking 
+{
+	public double Checkmon;
+	public double deposit(double p)
+	{
+		Checkmon=
+				Checkmon+p;
+		return Checkmon;
+	}
+	public double with(double q)
+	{
+		Checkmon=
+				Checkmon-q;
+		return Checkmon;
+	}
+}
