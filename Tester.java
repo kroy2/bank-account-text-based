73118222c4f1c2fde4c39f3839d198148c29af9d
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-07-15 15:50:03 -0600

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,16 @@
+package kanishka_roy;
+import java.util.*;
+import java.io.*;
+public class Tester extends Acc
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello, and thank you for joining this user friendly banking system. This system includes debit offers in terms of checkings and savings.");
+		Acc count=
+				new Acc();
+		while(true)
+		{
+			count.checking(); //checking/savings account
+		}
+	}
+}
