ca83a01fc150587fa291587713bdf2266d8b35fd
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-07-15 15:48:11 -0600

    Create Savings.java

---
+++ Savings.java
@@ -0,0 +1,26 @@
+package kanishka_roy;
+import java.io.*;
+import java.util.*;
+public class Savings 
+{
+	public double Savemon, inter;
+	public double in()
+	{
+		Savemon=
+				Savemon+inter;
+		return inter=
+				Savemon*0.05;
+	}
+	public double deposit(double pp)
+	{
+		Savemon=
+				Savemon+pp;
+		return Savemon;
+	}
+	public double with(double qq)
+	{
+		Savemon=
+				Savemon-qq;
+		return Savemon;
+	}
+}
