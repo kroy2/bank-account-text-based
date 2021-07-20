375c2b37e2d0468322dcbe01cf4e1ded0a6daf8f
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-07-20 17:04:28 -0600

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,26 +1,22 @@
 
 import java.io.*;
 import java.util.*;
-public class Savings 
-{
+
+public class Savings {
 	public double Savemon, inter;
-	public double in()
-	{
-		Savemon=
-				Savemon+inter;
-		return inter=
-				Savemon*0.05;
+
+	public double in() {
+		Savemon = Savemon + inter;
+		return inter = Savemon * 0.05;
 	}
-	public double deposit(double pp)
-	{
-		Savemon=
-				Savemon+pp;
+
+	public double deposit(double pp) {
+		Savemon = Savemon + pp;
 		return Savemon;
 	}
-	public double with(double qq)
-	{
-		Savemon=
-				Savemon-qq;
+
+	public double with(double qq) {
+		Savemon = Savemon - qq;
 		return Savemon;
 	}
 }
