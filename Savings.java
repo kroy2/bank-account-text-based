7af5d69c83fdf0243c6cacf043ab554aecb7eb73
NuclearTea <87144559+NuclearTea@users.noreply.github.com>
2021-07-20 17:46:15 -0600

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,3 +1,4 @@
+package kanishka_roy;
 
 import java.io.*;
 import java.util.*;
@@ -5,16 +6,17 @@ import java.util.*;
 public class Savings {
 	public double Savemon, inter;
 
+	// Applied interest to the savings account balance
 	public double in() {
 		Savemon = Savemon + inter;
 		return inter = Savemon * 0.05;
 	}
-
+	//adding money to savings account
 	public double deposit(double pp) {
 		Savemon = Savemon + pp;
 		return Savemon;
 	}
-
+	//withdrawing money from savings account
 	public double with(double qq) {
 		Savemon = Savemon - qq;
 		return Savemon;
