cb16908d42e17fe727f95ecb7e38ef5c4279d664
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-07-20 17:04:50 -0600

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,16 +1,14 @@
 
 import java.util.*;
 import java.io.*;
-public class Tester extends Acc
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hello, and thank you for joining this user friendly banking system. This system includes debit offers in terms of checkings and savings.");
-		Acc count=
-				new Acc();
-		while(true)
-		{
-			count.checking(); //checking/savings account
+
+public class Tester extends Acc {
+	public static void main(String args[]) {
+		System.out.println(
+				"Hello, and thank you for joining this user friendly banking system. This system includes debit offers in terms of checkings and savings.");
+		Acc count = new Acc();
+		while (true) {
+			count.checking(); // checking/savings account
 		}
 	}
 }
