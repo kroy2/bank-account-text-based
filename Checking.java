2b1e9d0bb0a0ed27cb8cfceaac745239ebb739da
NuclearTea <87144559+NuclearTea@users.noreply.github.com>
2021-07-20 17:45:53 -0600

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,17 +1,19 @@
-
+package kanishka_roy;
 import java.io.*;
 import java.util.*;
 
 public class Checking {
 	public double Checkmon;
 
+	// deposit money into checking account
 	public double deposit(double p) {
 		Checkmon = Checkmon + p;
 		return Checkmon;
 	}
-
+	// withdraw money from checking account
 	public double with(double q) {
 		Checkmon = Checkmon - q;
 		return Checkmon;
 	}
 }
+
