2ed4dee045dc0ac10828e7705cd2bdb2d151bc04
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-07-20 17:04:04 -0600

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,19 +1,17 @@
 
 import java.io.*;
 import java.util.*;
-public class Checking 
-{
+
+public class Checking {
 	public double Checkmon;
-	public double deposit(double p)
-	{
-		Checkmon=
-				Checkmon+p;
+
+	public double deposit(double p) {
+		Checkmon = Checkmon + p;
 		return Checkmon;
 	}
-	public double with(double q)
-	{
-		Checkmon=
-				Checkmon-q;
+
+	public double with(double q) {
+		Checkmon = Checkmon - q;
 		return Checkmon;
 	}
 }
