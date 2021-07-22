0ff05753eae5f78dc6a74835790b7513b2ddd638
NuclearTea <87144559+NuclearTea@users.noreply.github.com>
2021-07-22 17:40:38 -0600

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,37 +1,46 @@
+package kanishka_roy;
+
 import java.io.*;
 import java.util.*;
 
 public class Savings {
 	public double Savemon, inter;
 
-	// Applied interest to the savings account balance, to be called in for Savings in Acc class
+	// Applied interest to the savings account balance, to be called in for Savings
+	// in Acc class
 	public double in() {
 		Savemon = Savemon + inter;
 		return inter = Savemon * 0.05;
 	}
-	//adding money to savings account, to be called in for Savings in Acc class
-	public double deposit(double pp) {
-		Savemon = Savemon + pp;
+
+	// adding money to savings account, to be called in for Savings in Acc class
+	public double deposit(double moneyIn) {
+		Savemon = Savemon + moneyIn;
 		return Savemon;
 	}
-	//withdrawing money from savings account, to be called in for Savings in Acc class
-	public double with(double qq) {
-		Savemon = Savemon - qq;
+
+	// withdrawing money from savings account, to be called in for Savings in Acc
+	// class
+	public double with(double moneyOut) {
+		Savemon = Savemon - moneyOut;
 		return Savemon;
 	}
-	public double InterestTracker() {
+
+	public void InterestTracker() {
 		Scanner days = new Scanner(System.in);
-		System.out.println("How many days in advance you like to calculate the interest rate? Please enter a positive integer.");
+		System.out.println(
+				"How many days in advance you like to calculate the interest rate? Please enter a positive integer.");
 		String daysInAdvance_s = days.next();
 		int daysInAdvance = Integer.parseInt(daysInAdvance_s);
-		int x = 0;
+		int x = 1;
+		double projectedMoney = Savemon;
 		while (x < daysInAdvance) {
-			in();
+			projectedMoney = projectedMoney * 1.05;
 			x++;
+
 		}
-		return inter;
-		
+		System.out.println("$" + projectedMoney + " at day " + daysInAdvance);
+
 	}
-	
 
 }
