eca9bc4d3a464ed740aac6fa865999d631a6d82b
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-07-21 18:07:24 -0600

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -2,13 +2,16 @@
 import java.util.*;
 import java.io.*;
 
+// program run from tester class
+// tester class includes initial introductory message upon using the program every time after running
 public class Tester extends Acc {
 	public static void main(String args[]) {
 		System.out.println(
 				"Hello, and thank you for joining this user friendly banking system. This system includes debit offers in terms of checkings and savings.");
 		Acc count = new Acc();
 		while (true) {
-			count.checking(); // checking/savings account
+			// calling for database function set in Acc class
+			count.database(); // checking/savings account
 		}
 	}
 }
