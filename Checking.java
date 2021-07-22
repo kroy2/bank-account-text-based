aef1a8ba93b4a38b679abf7fb35db39acd5d94e1
NuclearTea <87144559+NuclearTea@users.noreply.github.com>
2021-07-22 17:38:11 -0600

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,3 +1,4 @@
+package kanishka_roy;
 import java.io.*;
 import java.util.*;
 
@@ -5,13 +6,13 @@ public class Checking {
 	public double Checkmon;
 
 	// deposit money method to be called into database function in Acc class
-	public double deposit(double p) {
-		Checkmon = Checkmon + p;
+	public double deposit(double moneyIn) {
+		Checkmon = Checkmon + moneyIn;
 		return Checkmon;
 	}
 	// withdraw money method to be called into database function in Acc class
-	public double with(double q) {
-		Checkmon = Checkmon - q;
+	public double with(double moneyOut) {
+		Checkmon = Checkmon - moneyOut;
 		return Checkmon;
 	}
 }
