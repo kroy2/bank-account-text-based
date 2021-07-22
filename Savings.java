76a762eb6b41b9d9179cd4a6aeb3f580b6c54121
kroy2 <87046573+kroy2@users.noreply.github.com>
2021-07-21 18:06:55 -0600

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,22 +1,20 @@
-
-
 import java.io.*;
 import java.util.*;
 
 public class Savings {
 	public double Savemon, inter;
 
-	// Applied interest to the savings account balance
+	// Applied interest to the savings account balance, to be called in for Savings in Acc class
 	public double in() {
 		Savemon = Savemon + inter;
 		return inter = Savemon * 0.05;
 	}
-	//adding money to savings account
+	//adding money to savings account, to be called in for Savings in Acc class
 	public double deposit(double pp) {
 		Savemon = Savemon + pp;
 		return Savemon;
 	}
-	//withdrawing money from savings account
+	//withdrawing money from savings account, to be called in for Savings in Acc class
 	public double with(double qq) {
 		Savemon = Savemon - qq;
 		return Savemon;
