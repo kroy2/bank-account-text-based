5c0a3743087d6d712b2e38517fe790ccbe13442e
NuclearTea <87144559+NuclearTea@users.noreply.github.com>
2021-07-22 23:25:07 -0600

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -17,7 +17,7 @@ public class Chequing {
 		return ChequingMoney;
 	}
 	// withdraw money method to be called into database function in Acc class
-	public double with(double moneyOut) {
+	public double withdraw(double moneyOut) {
 		ChequingMoney = ChequingMoney - moneyOut;
 		return ChequingMoney;
 	}
