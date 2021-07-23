f5fdcfea2427e1ae6e49c5481f01f1644b325818
NuclearTea <87144559+NuclearTea@users.noreply.github.com>
2021-07-22 21:59:02 -0600

    Add files via upload

---
+++ Bill.java
@@ -0,0 +1,55 @@
+package kanishka_roy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Not implemented yet in Iteration 1 but are planning to by giving the user the
+ * option to create a bill which has a price and frequency Program will auto
+ * remove money from chosen account If deduction will cause account to go to 0
+ * or below, will display error
+ */
+public class Bill {
+	ArrayList<Bill> BillsList = new ArrayList<Bill>();
+	String Billname = "";
+	double pricePerfrequency = 0;
+	int frequency = 0;
+
+	// Creates a Bill which has 3 elements
+	// Name, price every set time period and the time period
+	public Bill(String Billname, double pricePerfrequency, int frequency) {
+	}
+
+	public void setBillname() {
+		Scanner NameOfBill = new Scanner(System.in);
+		System.out.println("What is the name of the Bill.");
+		Billname = NameOfBill.next();
+	}
+
+	public String getBillname() {
+		return Billname;
+	}
+
+	public void setBillprice() {
+		Scanner BillPrice = new Scanner(System.in);
+		System.out.println("What is the price of the bill every instance?");
+		pricePerfrequency = (double) Integer.parseInt(BillPrice.next()); // Converting response to integer, and casting
+																			// it to a double
+	}
+
+	public double getBillprice() {
+		return pricePerfrequency;
+	}
+
+	public void setBillfrequency() {
+		Scanner Billfrequency = new Scanner(System.in);
+		System.out.println("How often is this bill charged? Respond in days (integers only).");
+		frequency = Integer.parseInt(Billfrequency.next()); // Converting string response to integer
+
+	}
+
+	public int getBillfrequency() {
+		return frequency;
+	}
+
+}
